0563aa4c423ad221c3958e2909ee2d6427b47f80
Kyle Berry <4wd22r@gmail.com>
2011-08-21 19:40:20 -0500

    fixed dumb fuckup

--- newrepo.sh
+++ newrepo.sh
@@ -2,9 +2,9 @@
 # Creates a bare git repo in the current directory
 # uses a command line argument to name the repo
 # designed for server usage.
-if [ $# -ne 1]; then #                      #If no argumnts, print an error message
-    echo Usage: $(basename $0) Repo name, no .git
-    exit
+if [ $# -ne 1 ]; then #                      
+    echo Usage: $(basename $0) reponame
+    exit 1
 fi
 
 mkdir ./$1.git                              # Make the new directory
