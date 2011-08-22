f25de34519fc0696d43cc0694638dd05d4a6d8ed
Kyle Berry <4wd22r@gmail.com>
2011-08-21 19:17:32 -0500

    error

--- newrepo.sh
+++ newrepo.sh
@@ -2,7 +2,7 @@
 # Creates a bare git repo in the current directory
 # uses a command line argument to name the repo
 # designed for server usage.
-if [ $# -ne 1]; then                        #If no argumnts, print an error message
+if [ $# -ne 1]; then #                      #If no argumnts, print an error message
     echo Usage: $(basename $0) Repo name, no .git
     exit
 fi
@@ -11,4 +11,4 @@ mkdir ./$1.git                              # Make the new directory
 cd ./$1.git
 git --bare init                             # Initalize bare repo
 echo "[gitweb]" >> config                   # Echo some lines into the config
-echo "      Owner = Kyle Berry" >> config   # for gitweb
+echo "        Owner = Kyle Berry" >> config # for gitweb
