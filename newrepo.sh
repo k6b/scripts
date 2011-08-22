68034a069128df06f937abcf8e5c1e0f28010f3d
Kyle Berry <4wd22r@gmail.com>
2011-08-21 19:57:53 -0500

    adding options to newrepo

--- newrepo.sh
+++ newrepo.sh
@@ -2,8 +2,8 @@
 # Creates a bare git repo in the current directory
 # uses a command line argument to name the repo
 # designed for server usage.
-if [ $# -ne 1 ]; then #                      
-    echo Usage: $(basename $0) reponame
+if [ $# -ne 3 ]; then #                      
+    echo "Usage: $(basename $0) reponame description export(yes/no)"
     exit 1
 fi
 
@@ -12,3 +12,13 @@ cd ./$1.git
 git --bare init                             # Initalize bare repo
 echo "[gitweb]" >> config                   # Echo some lines into the config
 echo "        Owner = Kyle Berry" >> config # for gitweb
+echo $2 > description                       # Set repo description
+
+if [$3 == "yes" ]; then
+    echo "Ok to export to gitweb"
+    touch git-daemon-export-ok
+else
+    echo "Not ok to export to gitweb"
+fi
+
+chown -R git:git ./$1
