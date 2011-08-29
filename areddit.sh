b080ce8e4c0034719e929ff2465704267cc72488
Kyle Berry <4wd22r@gmail.com>
2011-08-28 19:53:41 -0500

    areddit.sh, change update time add screenshot.sh for scrotwm

--- areddit.sh
+++ areddit.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
-karma=$(curl --connect-timeout 1 -fsm 3 http://www.reddit.com/user/4wd22r/about.json | awk '{match($0, "k_karma\": ([0-9]+)", a); match($0, "t_karma\": ([0-9]+)", b); print "L:", a[1], "C:", b[1];}')
-echo "redditwidget.text = '$karma'" | awesome-client
+sleep_sec=900
+count=0
+while :; do
+    let count=$count+1
+    karma=$(curl --connect-timeout 1 -fsm 3 http://www.reddit.com/user/4wd22r/about.json | awk '{match($0, "k_karma\": ([0-9]+)", a); match($0, "t_karma\": ([0-9]+)", b); print "L:", a[1], "C:", b[1];}')
+    echo "redditwidget.text = '$karma'" | awesome-client
+    sleep $sleep_sec
+done
