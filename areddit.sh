26271a83a7e5108455902923462d0234c6fa9b85
Kyle Berry <4wd22r@gmail.com>
2011-08-28 19:56:08 -0500

    added  var

--- areddit.sh
+++ areddit.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
+user=4wd22r
 sleep_sec=900
 count=0
 while :; do
     let count=$count+1
-    karma=$(curl --connect-timeout 1 -fsm 3 http://www.reddit.com/user/4wd22r/about.json | awk '{match($0, "k_karma\": ([0-9]+)", a); match($0, "t_karma\": ([0-9]+)", b); print "L:", a[1], "C:", b[1];}')
+    karma=$(curl --connect-timeout 1 -fsm 3 http://www.reddit.com/user/$user/about.json | awk '{match($0, "k_karma\": ([0-9]+)", a); match($0, "t_karma\": ([0-9]+)", b); print "L:", a[1], "C:", b[1];}')
     echo "redditwidget.text = '$karma'" | awesome-client
     sleep $sleep_sec
 done
