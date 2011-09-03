0a072e044636f3b0341c23a6fb73423c688a524c
Kyle Berry <4wd22r@gmail.com>
2011-09-03 15:42:22 -0500

    cleaning up spacing

--- bancount.sh
+++ bancount.sh
@@ -4,4 +4,4 @@ awk "/\ Ban / "'{ nmatches++ }
     END { print nmatches, "IPs have been banned." }' /var/log/fail2ban.log
 awk "/Ban [0-9]++.[0-9]++.[0-9]++.[0-9]++/"'{a[$7]++}
     { for ( i in a ) { if ( a[i]-1 ) print i, a[i]}}' /var/log/fail2ban.log |
-awk 'END{ if ($2>1) { print $1" has been banned "$2" times." }}'
+awk 'END { if ($2>1) { print $1" has been banned "$2" times." }}'
