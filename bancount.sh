67e355ca618627a8256266d73615736804f1f091
Kyle Berry <4wd22r@gmail.com>
2011-09-02 23:29:31 -0500

    more awk goodness

--- bancount.sh
+++ bancount.sh
@@ -1,7 +1,4 @@
 #! /bin/sh
 # short script to find the number of IPs banned by fail2ban
-touch tmpfile
-awk '{if ($6=="Ban") {print $6"\t"$7}}' /var/log/fail2ban.log > tmpfile
-bans=$(cat tmpfile | wc -l)
-rm tmpfile
-echo "$bans IPs have been banned."
+awk "/\ Ban / "'{ nmatches++ }
+    END { print nmatches, "IPs have been banned." }' /var/log/fail2ban.log
