65295b2d706ec68b817da3c39ed530c68248f905
Kyle Berry <4wd22r@gmail.com>
2011-09-03 04:15:33 -0500

    delete banips.sh

--- banips.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-# short script to find the number of IPs banned by fail2ban
-touch tmpfile
-awk '{print $6"\t"$7}' /var/log/fail2ban.log > tmpfile
-cat tmpfile | grep Ban | awk '{print $2}' 
-rm tmpfile
