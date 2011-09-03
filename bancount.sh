808abd0d5f229ace262f2dd5d74962fdf09bc4d7
Kyle Berry <4wd22r@gmail.com>
2011-09-03 04:13:25 -0500

    added bit to count repeat offenders

--- bancount.sh
+++ bancount.sh
@@ -2,3 +2,6 @@
 # short script to find the number of IPs banned by fail2ban
 awk "/\ Ban / "'{ nmatches++ }
     END { print nmatches, "IPs have been banned." }' /var/log/fail2ban.log
+awk "/Ban [0-9]++.[0-9]++.[0-9]++.[0-9]++/"'{a[$7]++}
+    {for(i in a){if(a[i]-1)print i,a[i]}}' /var/log/fail2ban.log |
+awk 'END{if ($2>1) {print $1" has been banned "$2" times."}}'
