33dd371405b433baa4edfa80e0b6b1a4ba10fadc
Kyle Berry <4wd22r@gmail.com>
2011-09-12 00:17:24 -0500

    add trayer to startup

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
+trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 4 --transparent true --tint 0x191970 --height 12 &
 xsetroot -cursor_name left_ptr
 feh --bg-scale /usr/share/archlinux/wallpaper/archlinux-simplyblack.png
