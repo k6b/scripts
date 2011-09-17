bef7e97a60183d8f0e69c98c41e509bd82be8d8d
Kyle Berry <4wd22r@gmail.com>
2011-09-17 17:49:24 -0500

    cpu temp script

---
+++ temp.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+tempc=$(echo " scale=1 ; ( $temp / 1000 ) " | bc )
+tempf=$(echo " scale=1 ; ( ( 9 / 5 ) * $tempc ) + 32" | bc )
+echo Temp: $tempf°
