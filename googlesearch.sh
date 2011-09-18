fe551e86e7206415dface108a49a8e81adabffcd
Kyle Berry <4wd22r@gmail.com>
2011-09-18 03:48:10 -0500

    googlesearch

---
+++ googlesearch.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+term="test+search"
+sleep_sec=7200
+count=0
+while :; do
+    let count=$count+1
+    wget -U "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7) Gecko/20040613 Firefox/0.8.0+" "http://www.google.com/search?q=$term"
+#    rm search\?q=$term
+    sleep $sleep_sec
+done
