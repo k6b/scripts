de2f3cb74f165c8eda4ed5ada0c53dbf51c554c8
Kyle Berry <4wd22r@gmail.com>
2011-09-19 03:38:11 -0500

    remove comment

--- googlesearch.sh
+++ googlesearch.sh
@@ -5,6 +5,6 @@ count=0
 while :; do
     let count=$count+1
     wget -U "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7) Gecko/20040613 Firefox/0.8.0+" "http://www.google.com/search?q=$term"
-#    rm search\?q=$term
+    rm search\?q=$term
     sleep $sleep_sec
 done
