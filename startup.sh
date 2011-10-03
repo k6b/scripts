e54070222e57e1e0ae3f76f6a26fb7ce4a8692d0
Kyle Berry <4wd22r@gmail.com>
2011-10-02 23:43:39 -0500

    change background

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 xsetroot -cursor_name left_ptr
-feh --bg-scale /usr/share/archlinux/wallpaper/archlinux-simplyblack.png
+#feh --bg-scale /usr/share/archlinux/wallpaper/archlinux-simplyblack.png
+feh --bg-scale /home/k6b/pictures/xmonadwallarchlogo.png
