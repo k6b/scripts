8cd636ec0ac13db64055a362ed37ab4f4b68ae47
Kyle Berry <4wd22r@gmail.com>
2011-10-02 23:45:06 -0500

    change timeout to 3 seconds

--- xreddit.sh
+++ xreddit.sh
@@ -4,7 +4,7 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-curl --connect-timeout 1 -fsm 3 http://www.reddit.com/user/$1/about.json | 
+curl --connect-timeout 3 -fsm 3 http://www.reddit.com/user/$1/about.json | 
 awk '{match($0, "k_karma\": ([0-9]+)", a);
       match($0, "t_karma\": ([0-9]+)", b);
       print "L:", a[1], "C:", b[1];}'
