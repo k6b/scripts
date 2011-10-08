50cbccdce16cbcf1bff3ae3fd3a2f4a1a0c7568b
Kyle Berry <4wd22r@gmail.com>
2011-10-08 15:47:54 -0500

    rsync script

---
+++ sync.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+remote_server="kylefberry.net"
+remote_user="kyleb"
+local_dir="/srv/http/"
+remote_dir="/home/kyleb/public_html"
+key_file="/home/k6b/.ssh/id_rsa2"
+log_file="/home/k6b/rsync_log"
+date=`date +"%m-%d-%y %T"`
+
+echo $date >> $log_file
+rsync -ave "ssh -i $key_file" $local_dir $remote_user@$remote_server:$remote_dir >> $log_file
