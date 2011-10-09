0465ddab14ccba9ac9e915e9b38ec6436ab59a0d
Kyle Berry <4wd22r@gmail.com>
2011-10-09 00:31:15 -0500

    testing

--- proccomment.sh
+++ proccomment.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-#name=$(awk "/post_id:/"'{print $2}' $1 | awk 'BEGIN { FS = "/" } { print $5 }')
+#name=`awk "/post_id:/"'{print $2}' $1 | awk 'BEGIN { FS = "/" } { print $5 }'`
+#date=`awk "/date:/"'{print $2"-"$3}' $1`
 name=$RANDOM
 date=$(date +"%m-%d-%Y") 
 sed '0,/From:/d' $1 > /home/k6b/jekyll/_comments/$date-$name
