d2b4ceb41b9aa940289054ea22bca3c822689006
Kyle Berry <4wd22r@gmail.com>
2011-10-09 14:06:16 -0500

    change timestamp

--- sync.sh
+++ sync.sh
@@ -8,5 +8,5 @@ key_file="/home/k6b/.ssh/id_rsa2"
 log_file="/home/k6b/rsync_log"
 date=`date +"%m-%d-%y %T"`
 
-echo $date >> $log_file
+echo [$date] >> $log_file
 rsync -ave "ssh -i $key_file" $local_dir $remote_user@$remote_server:$remote_dir >> $log_file
