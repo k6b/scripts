80117ffcfdf1370a20b04e3d65ac51ec985ac547
Kyle Berry <4wd22r@gmail.com>
2011-10-12 02:37:50 -0500

    fix script to show more than one IP that's been banned more than once

--- bancount.sh
+++ bancount.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
 # short script to find the number of IPs banned by fail2ban
+echo -e '\n'
 awk "/\ Ban / "'{ nmatches++ }
     END { print nmatches, "IPs have been banned." }' /var/log/fail2ban.log
-awk "/Ban [0-9]++.[0-9]++.[0-9]++.[0-9]++/"'{ a[$7]++ }
-    { for ( i in a ) { if ( a[i]-1 ) print i, a[i] }}' /var/log/fail2ban.log |
-awk 'END { if ($2>1) { print $1" has been banned "$2" times." }}'
+echo -e '\n'IP'\t\t'Bans
+awk "/Ban [0-9]++.[0-9]++.[0-9]++.[0-9]++/"'{ a[$7]++ } 
+    { for ( i in a ) { if ( a[i]-1 ) print i, a[i] }}' /var/log/fail2ban.log | 
+awk '! a[$0]++' | 
+awk '{a[$1]<$NF?a[$1]=$NF:""} END {for (i in a){print i,"\t"a[i]}}'
+echo -e '\n'
