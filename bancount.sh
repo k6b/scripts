199eeafbffdbc667416cdead93501d25178de6fa
Kyle Berry <4wd22r@gmail.com>
2011-10-12 03:00:45 -0500

    fix spacing

--- bancount.sh
+++ bancount.sh
@@ -10,5 +10,5 @@ awk "/\ Ban / "'{ nmatches++ }
 echo -e '\n'"\033[4m\033[1mIP\t\tBans\033[0m"
 awk "/Ban [0-9]++.[0-9]++.[0-9]++.[0-9]++/"'{ a[$7]++ } 
     { for ( i in a ) { if ( a[i]-1 ) print i, a[i] }}' /var/log/fail2ban.log | 
-awk '{a[$1]<$NF?a[$1]=$NF:""} END {for (i in a){print i,"\t"a[i]}}'
+awk '{ a[$1] < $NF? a[$1] = $NF : "" } END { for ( i in a ) { print i, "\t" a[i] }}'
 echo 
