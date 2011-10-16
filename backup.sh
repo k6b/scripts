8dc30579a1c18d2de5bb8482a6a6ff28bf78d878
Kyle Berry <4wd22r@gmail.com>
2011-10-16 04:47:01 -0500

    update

--- backup.sh
+++ backup.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 #Thanks Spuh!
+echo $(date) >> /var/log/backup_log
 find /backup -type f -iname "backup*" -mtime +14 -exec rm {} \;
 for i in $(grep ^backups /etc/group |cut -d: -f 4- |sed s/,/\ /g)
 do
     grep $i /etc/passwd | awk -F : '{print $6}'
-    tar zcvf /backup/backup.$(date +%m.%d.%y).$i.tar.gz $(grep $i /etc/passwd | awk -F : '{print $6}')
-    chown $i:$i backup.`date +%m.%d.%y`.$i.tar.gz  
+    tar zcf /backup/backup.$(date +%m.%d.%y).$i.tar.gz $(grep $i /etc/passwd | awk -F : '{print $6}')
+    chown $i:$i /backup/backup.$(date +%m.%d.%y).$i.tar.gz  
 done
