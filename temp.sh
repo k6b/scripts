b0cba7eb377fe20a9d573f9c0ab417a02a95ec5a
Kyle Berry <4wd22r@gmail.com>
2011-10-16 01:37:05 -0500

    change to sh

--- temp.sh
+++ temp.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 temp=$(cat /sys/class/thermal/thermal_zone0/temp)
 tempc=$(echo " scale=1 ; ( $temp / 1000 ) " | bc )
 tempf=$(echo " scale=1 ; ( ( 9 / 5 ) * $tempc ) + 32" | bc )
