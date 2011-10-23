a84fca5ca296eebc1d73cfabf758dd8d7905aec7
Kyle Berry <4wd22r@gmail.com>
2011-10-23 18:47:58 -0500

    add geoip lookup for both lists

--- bancount.sh
+++ bancount.sh
@@ -12,11 +12,27 @@ echo -e '\n'"\033[4m\033[1mFail2Ban\033[0m"'\n'
 awk "/\ Ban / "'{ nmatches++ }
     END { print nmatches, "IPs have been banned." }' /var/log/fail2ban.log
 
-echo -e '\n'"\033[4mIP\t\tBans\033[0m"
+echo -e '\n'"\033[4mIP\t\tBans\tCountry\033[0m"
 
-awk "/Ban [0-9]++.[0-9]++.[0-9]++.[0-9]++/"'{ a[$7]++ } 
+iplist=$(awk "/Ban [0-9]++.[0-9]++.[0-9]++.[0-9]++/"'{ a[$7]++ } 
     { for ( i in a ) { if ( a[i]-1 ) print i, a[i] }}' /var/log/fail2ban.log | 
-awk '{ a[$1] < $NF? a[$1] = $NF : "" } END { for ( i in a ) { print i, "\t" a[i] }}'
+awk '{ a[$1] < $NF? a[$1] = $NF : "" } END { for ( i in a ) { print i, "\t" a[i] }}' |
+awk '{print $1}') 
+
+for i in $iplist
+do
+ip=$(awk "/Ban\ $i/"'{ a[$7]++ } 
+    { for ( i in a ) { if ( a[i]-1 ) print i, a[i] }}' /var/log/fail2ban.log | 
+awk '{ a[$1] < $NF? a[$1] = $NF : "" } END { for ( i in a ) { print i, "\t" a[i] }}' |
+awk '{print $1}')
+
+bantime=$(awk "/Ban\ $i/"'{ a[$7]++ } 
+    { for ( i in a ) { if ( a[i]-1 ) print i, a[i] }}' /var/log/fail2ban.log | 
+awk '{ a[$1] < $NF? a[$1] = $NF : "" } END { for ( i in a ) { print i, "\t" a[i] }}' |
+awk '{print $2}')
+
+echo -e $ip'\t'$bantime'\t'$(geoiplookup $i | awk -F , '{print $2}' | sed s/\ //)
+done
 
 echo
 
@@ -35,7 +51,7 @@ if [[ $total -ne "0" ]]
 then
     ips=$(iptables -L | awk "/DROP\ /"'{ print $4}' | awk '{ print $1 }')
     echo -e "\033[4mCurrently Banned\033[0m"'\n'
-    echo -e "\033[4mIP\t\tDate\t\tTime\033[0m"
+    echo -e "\033[4mIP\t\tDate\t\tTime\t\tCountry\033[0m"
         for i in $ips
         do
             if [[ $i =~ [0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3} ]]
@@ -46,7 +62,8 @@ then
             fi
             date=$(awk "/Ban\ $i/"'{ print $1 }' /var/log/fail2ban.log)
             time=$(awk "/Ban\ $i/"'{ print $2 }' /var/log/fail2ban.log | cut -d , -f 1)
-            echo -e $i'\t'$date'\t'$time
+            country=$(geoiplookup $i | awk '{print $5}')
+            echo -e $i'\t'$date'\t'$time'\t'$country
         done
     echo
 fi
