ab36052fbb29b5ef818755d40e09c8a27005ede4
Kyle Berry <4wd22r@gmail.com>
2011-10-23 03:05:44 -0500

    remove temp file bullshit

--- banlist.sh
+++ banlist.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
 
-awk "/\ Ban [0-9]++.[0-9]++.[0-9]++.[0-9]++/"'{print $1,$2}' /var/log/fail2ban.log | cut -d , -f 1 > ./.tmp 
+awk "/\ Ban [0-9]++.[0-9]++.[0-9]++.[0-9]++/"'{print $1,$2}' /var/log/fail2ban.log | cut -d , -f 1 > ./$1 
 
-awk '{print $2,$1}' ./.tmp > ./$1
 
 chown k6b:k6b ./$1
