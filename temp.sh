d82b72843a819f25e28a7341936c87ee7965c714
Kyle Berry <4wd22r@gmail.com>
2011-10-29 17:55:43 -0500

    add colors

--- temp.sh
+++ temp.sh
@@ -2,4 +2,12 @@
 temp=$(cat /sys/class/thermal/thermal_zone0/temp)
 tempc=$(echo " scale=1 ; ( $temp / 1000 ) " | bc )
 tempf=$(echo " scale=1 ; ( ( 9 / 5 ) * $tempc ) + 32" | bc )
-echo Temp: $tempf°
+if [[ $tempf < 130 ]]
+then
+    echo -e "Temp: \e[32m$tempf°\e[0m"
+elif [[ $tempf > 150 ]]
+then
+    echo -e "Temp: \e[31m$tempf°\e[0m"
+else
+    echo -e "Temp: \e[33m$tempf°\e[0m"
+fi
