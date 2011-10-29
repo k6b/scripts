89e4d0126726edd6057452b58741936c513245da
Kyle Berry <4wd22r@gmail.com>
2011-10-29 17:32:34 -0500

    add fancy colors

--- battery.sh
+++ battery.sh
@@ -2,4 +2,18 @@
 
 battery=$(acpi | awk '{print $4,$3,$5}' | sed s/,//g)
 
-echo $battery
+percent=$(echo $battery | awk '{print $1}')
+state=$(echo $battery | awk '{print $2}')
+time=$(echo $battery | awk '{print $3}')
+
+echo $percent $state $time
+
+if [[ $percent > "75%" ]]
+then
+    echo -e "Batt: \e[32m$percent\e[0m $state $time"
+elif [[ $percent < "25%" ]]
+then
+    echo -e "Batt: \e[31m$percent\e[0m $state $time"
+else
+    echo -e "Batt: \e[33m$percent\e[0m $state $time"
+fi
