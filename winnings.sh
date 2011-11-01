69550138809e1f59cc4bec63a44ded2d421bf147
Kyle Berry <4wd22r@gmail.com>
2011-11-01 02:03:24 -0500

    add full path, and conditional

--- winnings.sh
+++ winnings.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 
-money=$(grep -i "congratulations $1" \#austinreddit.log | awk '{print $6}' | sed -e 's/,\|\$//g' | cut -d . -f 1)
+money=$(grep -i "congratulations $1" ~/irclogs/FreeNode/\#austinreddit.log | awk '{print $6}' | sed -e 's/,\|\$//g' | cut -d . -f 1 | grep -e [0-9])
 
 cash=$((echo $money | sed 's/\ /\ +\ /g') | bc)
 
-echo -e You have won: \$$cash
+if [[ $cash < 1 ]]
+then
+    echo $1 has won NOTHING!
+else
+    echo -e $1 has won: \$$cash
+fi
