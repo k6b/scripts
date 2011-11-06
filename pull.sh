033805093bc3438e15064a79b5153bbdbc0cddd0
Kyle Berry <4wd22r@gmail.com>
2011-11-06 05:41:53 -0600

    change sleep time

--- pull.sh
+++ pull.sh
@@ -6,9 +6,10 @@ bigone=25
 if [ "$1" -lt "$bigone" ]
 then
    echo Get ready! Pulling $1 times!!
+   sleep 2
 else
     echo This is gonna be a BIG ONE!
-    sleep 1
+    sleep 2
     echo Pulling $1 times!!!
     sleep 1
 fi
