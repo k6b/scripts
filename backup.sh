5e6171d0dd3bf5832cfab250854b35ac57d49259
Kyle Berry <4wd22r@gmail.com>
2011-11-06 05:41:25 -0600

    fix log location

--- backup.sh
+++ backup.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 #Thanks Spuh!
-echo $(date) >> /var/log/backup_log
+echo $(date) >> /var/log/backup.log
+
+
+
 find /backup -type f -iname "backup*" -mtime +14 -exec rm {} \;
+
+
+
 for i in $(grep ^backups /etc/group |cut -d: -f 4- |sed s/,/\ /g)
 do
     grep $i /etc/passwd | awk -F : '{print $6}'
