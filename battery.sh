a424c5d3f5882ec350dd0dc566e24c8df15253cd
Kyle Berry <4wd22r@gmail.com>
2011-11-06 06:27:10 -0600

    fix color output

--- battery.sh
+++ battery.sh
@@ -2,16 +2,16 @@
 
 battery=$(acpi | awk '{print $4,$3,$5}' | sed s/,//g)
 
-percent=$(echo $battery | awk '{print $1}')
+percent=$(echo $battery | awk '{print $1}' | sed 's/%//')
 state=$(echo $battery | awk '{print $2}')
 time=$(echo $battery | awk '{print $3}')
 
-if [[ $percent > "75%" ]]
+if [[ $percent -gt "66" ]]
 then
-    echo -e "Batt: \e[32m$percent\e[0m $state $time"
-elif [[ $percent < "25%" ]]
+    echo -e "Batt: \e[32m$percent%\e[0m $state $time"
+elif [[ $percent < "33%" ]]
 then
-    echo -e "Batt: \e[31m$percent\e[0m $state $time"
+    echo -e "Batt: \e[31m$percent%\e[0m $state $time"
 else
-    echo -e "Batt: \e[33m$percent\e[0m $state $time"
+    echo -e "Batt: \e[33m$percent%\e[0m $state $time"
 fi
