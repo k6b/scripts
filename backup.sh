1408452d47b92c383f3a9b26c4b75c4ebe9ee335
Kyle Berry <4wd22r@gmail.com>
2011-11-11 05:33:46 -0600

    integrate with s3

--- backup.sh
+++ backup.sh
@@ -2,12 +2,6 @@
 #Thanks Spuh!
 echo $(date) >> /var/log/backup.log
 
-
-
-find /backup -type f -iname "backup*" -mtime +14 -exec rm {} \;
-
-
-
 for i in $(grep ^backups /etc/group |cut -d: -f 4- |sed s/,/\ /g)
 do
     grep $i /etc/passwd | awk -F : '{print $6}'
@@ -19,3 +13,6 @@ for j in $sites
 do
     tar zcf /backup/backup.$(date +%m.%d.%y).$j.tar.gz /srv/$j
 done
+
+find /backup -type f -iname "backup*" -mtime +3 -exec s3cmd -p put {} s3://k6b-backups/ \;
+find /backup -type f -iname "backup*" -mtime +3 -delete
