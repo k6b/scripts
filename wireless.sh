1b3d97aa1074744579e3bf1e6925ee46ed79566b
Kyle Berry <4wd22r@gmail.com>
2011-11-15 05:33:47 -0600

    no need to check for wireless extensions, there is only wireless on the cr-48

--- wireless.sh
+++ wireless.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-iwconfig wlan0 2>&1 | grep -q no\ wireless\ extensions\. && {
-  echo wired
-    exit 0
-}
-
-essid=`iwconfig wlan0 | awk -F '"' '/ESSID/ {print $2}'`
-stngth=`iwconfig wlan0 | awk -F '=' '/Quality/ {print $2}' | cut -d '/' -f 1`
-bars=`expr $stngth / 7`
+essid=$(iwconfig wlan0 | awk -F '"' '/ESSID/ {print $2}')
+stngth=$(iwconfig wlan0 | awk -F '=' '/Quality/ {print $2}' | cut -d '/' -f 1)
+bars=$(expr $stngth / 7)
 
 case $bars in
   1) bar='{/---------}' ;;
