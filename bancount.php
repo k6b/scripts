8278003d0b3b8a77aeda7ea5891970fd2b47c939
Kyle Berry <4wd22r@gmail.com>
2012-01-28 16:49:31 -0600

    fixed all the css issues page is now valid html5 and css

--- bancount.php
+++ bancount.php
@@ -1,42 +1,13 @@
+<!DOCTYPE html>
 <html><head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title>Fail2BanCount - by k6b</title>
-<style type="text/css">
-/* <![CDATA[ */
-
-	body {
-		margin:0; padding:0;
-		font-size:80%;
-		font-family: sans-serif;
-		}
-
-	#header {
-		display: block;
-		width: 80%;
-		margin: auto;
-		}
-
-	#container {
-		width: 80%;
-		margin: auto;
-		padding:0;
-		display: table;
-		}
-
-	#row  {
-		display: table-row;
-		}
-
-	#cell {
-		width:150px;
-		padding:1em;
-		background: #FFF;
-		display: table-cell;
-		}
-
-/* ]]> */
-</style>
-</head><body><center>
+<meta name="author" content="k6b" />
+<meta name="description" content="Fail2BanCount - by k6b" />
+<link rel="stylesheet" href="/bancount.css" type="text/css" />
+</head>
+<body>
+<div id="header"> 
 <?php
 
 // Fail2BanCount - Displays information from the database
@@ -112,42 +83,31 @@ $currentlybanned = $numbans - $numunbans;
 
 // Print some HTML
 
-echo "<div id='container'>";
-
-echo "<center>";
-
-echo "<h1><u>Fail2BanCount - by k6b</u></h1>";
-echo "<h3>$numbans IPs have been banned.</h3>";
+echo "\t<h1><u>Fail2BanCount - by k6b</u></h1>\n";
+echo "</div>\n";
+echo "<div id='container'>\n";
+echo "\t<h3>$numbans IPs have been banned.</h3>\n";
 
 // Begin creating the first table of IPs that have been banned
 // more than once.
 
-//echo "<table border='1'><tr><th>IP</th><th>Bans</th><th>Country</th>";
-
-echo "<div id='row'>";
-echo "<div id='cell'><b><u>IP</u></b></div><div id='cell'><b><u>Bans</u></b></div><div id='cell'><b><u>Country</u></b></div>";
+echo "\t<div class='table'>\n";
+echo "\t\t<div class='row'>\n";
+echo "\t\t\t<div class='cell'><b><u>IP</u></b></div>\n\t\t\t<div class='cell'><b><u>Bans</u></b></div>\n\t\t\t<div class='cell'><b><u>Country</u></b></div>\n";
+echo "\t\t</div>\n";
 
 // Print the data obtained from the MySQL database
 
-$multiplebans_fields = mysql_num_fields($multiplebans);
-
 // Print the first table
 
-for($i=3; $i<$multiplebans_fields; $i++) {
-	$field = mysql_fetch_field($multiplebans);
-	echo "<div id='cell'>{$field->name}</div>";
-}
-echo "</div>\n";
-
-// Print out the tables rows
-
 while($row = mysql_fetch_row($multiplebans)) {
-	echo '<div id="row">';
+	echo "\t\t<div class='row'>\n";
 	foreach($row as $cell)
-		echo "<div id='cell'>$cell</div>";
-	echo "</div>\n";
+		echo "\t\t\t<div class='cell'>$cell</div>\n";
+	echo "\t\t</div>\n";
 }
-echo "</div>";
+
+echo "\t</div>\n";
 
 mysql_free_result($multiplebans);
 
@@ -159,7 +119,7 @@ if ($currentlybanned != 1) {
 	$grammer = "IP is";
 }
 
-echo "<h3>Currently $currentlybanned $grammer banned.</h3>";
+echo "\t<h3>Currently $currentlybanned $grammer banned.</h3>\n";
 
 // Only print the second table if we have an IP
 // currently banned.
@@ -168,81 +128,63 @@ if ($numbans > $numunbans) {
 
 // Print the data obtained from the MySQL database
 
-$currentlybanned_fields = mysql_num_fields($currentbans);
-
 // Table title
 
-echo "<h2><u>Currently Banned</u></h2>";
+echo "\t<h2><u>Currently Banned</u></h2>\n";
 
 // Create the second table, of currently banned IPs
 
-//echo "<table border='1'><tr><th>IP</th><th>Date</th><th>Time</th><th>Country</th>";
-
-echo "<div id='row'>";
-echo "<div id='cell'><b><u>IP</u></b></div><div id='cell'><b><u>Date</u></b></div><div id='cell'><b><u>Time</u></b></div><div id='cell'><b><u>Country</u></b></div>";
+echo "\t<div class='table'>\n";
+echo "\t\t<div class='row'>\n";
+echo "\t\t\t<div class='cell'><b><u>IP</u></b></div>\n\t\t\t<div class='cell'><b><u>Date</u></b></div>\n\t\t\t<div class='cell'><b><u>Time</u></b></div>\n\t\t\t<div class='cell'><b><u>Country</u></b></div>\n";
+echo "\t\t</div>\n";
 
 // Print out the second table 
 
-for($i=4; $i<$currentlybanned_fields; $i++) {
-	$field = mysql_fetch_field($currentbans);
-	echo "<div id='cell'>{$field->name}</div>";
-}
-echo "</div>\n";
-
-// Print out the second table's rows
-
 while($row = mysql_fetch_row($currentbans)) {
-	echo "<div id='row'>";
+	echo "\t\t<div class='row'>\n";
 	foreach($row as $cell)
-		echo "<div id='cell'>$cell</div>";
-	echo "</div>\n";
+		echo "\t\t\t<div class='cell'>$cell</div>\n";
+	echo "\t\t</div>\n";
 
-echo "</div>";
 }
+
+echo "\t</div>\n";
 
 mysql_free_result($currentbans);
 
 }
 
 // Print more HTML
-
-echo "<h2><u>Top 10 Countries</u></h2>";
+echo "\t<h2><u>Top 10 Countries</u></h2>\n";
 
 // Begin creating the second table of counrtys  that have been banned
 // more than once.
 
-//echo "<table border='1'><tr><th>Country</th><th>Bans</th>";
-
-echo "<div id='row'>";
-echo "<div id='cell'><b><u>Country</u></b></div><div id='cell'><b><u>Bans</u></b></div>";
-
-// Print the data obtained from the MySQL database
-
-$multiplebans_fields = mysql_num_fields($countrybans);
+echo "\t<div class='table'>\n";
+echo "\t\t<div class='row'>\n";
+echo "\t\t\t<div class='cell'><b><u>Country</u></b></div>\n\t\t\t<div class='cell'><b><u>Bans</u></b></div>\n";
+echo "\t\t</div>\n";
 
 // Print the first table
 
-for($i=2; $i<$countrybans_fields; $i++) {
-	$field = mysql_fetch_field($countrybans);
-	echo "<div id='cell'>{$field->name}</div>";
-}
-echo "</div>\n";
 
-// Print out the tables rows
 
 while($row = mysql_fetch_row($countrybans)) {
-	echo "<div id='row'>";
+	echo "\t\t<div class='row'>\n";
 	foreach($row as $cell)
-		echo "<div id='cell'>$cell</div>";
-	echo "</div>\n";
+		echo "\t\t\t<div class='cell'>$cell</div>\n";
+	echo "\t\t</div>\n";
 }
-echo "</div>";
+
+echo "\t</div>\n";
 
 mysql_free_result($countrybans);
 
-echo "<br /><br />";
-
 ?>
-</center>
 </div>
-</center></body></html>
+<div id="footer">
+	©2012 released under GNU GPL contact <a href="mailto:kyle@kylefberry.net">k6b</a> with any questions.
+</div>
+</body>
+</html>
