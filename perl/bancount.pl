8de2263259b43bcd3e945121114992e2f755f161
Kyle Berry <4wd22r@gmail.com>
2012-02-10 14:59:56 -0600

    better sql queries, fixed some spacing

--- perl/bancount.pl
+++ perl/bancount.pl
@@ -6,9 +6,9 @@ use DBI;
 use DBD::mysql;
 use Term::ANSIColor;
 
-my $mysqldb = "fail2bancount";
-my $mysqlpw = "OJ9tzckQyVqCLzut";
-my $mysqluser = "fail2bancount";
+my $mysqldb = "";
+my $mysqlpw = "";
+my $mysqluser = "";
 
 my $db = DBI->connect(
 	"DBI:mysql:database=$mysqldb",
@@ -22,21 +22,12 @@ my $unbans = $db->prepare("SELECT MAX(id) From unbans");
 my $multiplebans = $db->prepare("SELECT ip,COUNT(*) count,country FROM bans GROUP BY ip HAVING count > 1 ORDER BY count DESC");
 
 $currentbans->execute();
-$bans->execute();
-$unbans->execute();
 $multiplebans->execute();
 
 # find total bans
 
-while (my $ref = $bans->fetchrow_arrayref) {
-	our $totalban = $ref->[0];
-	}
-
-while (my $ref = $unbans->fetchrow_arrayref) {
-	our $totalunban = $ref->[0];
-	}
-
-our ($totalban,$totalunban);
+my $totalban = $db->selectrow_array($bans);
+my $totalunban = $db->selectrow_array($unbans);
 our $currentlybanned = $totalban - $totalunban;
 
 print color 'bold';
@@ -56,26 +47,22 @@ while (my $ref = $multiplebans->fetchrow_arrayref) {
 	print $ref->[0] . "\t" . $ref->[1] . "\t" . $ref->[2] . "\n";
 	}
 
-print "\n";
-
 if ($currentlybanned == 1) {
-	print "Currently $currentlybanned IP is banned.\n\n";
+	print "\nCurrently $currentlybanned IP is banned.\n\n";
 } else {
-	print "Currently $currentlybanned IPs are banned.\n\n";
+	print "\nCurrently $currentlybanned IPs are banned.\n\n";
 	}
 
 # Print current bans
 
 if ($currentlybanned > 0) {
-print color 'underline';
-print "Currently Banned\n\n";
+	print color 'underline';
+	print "Currently Banned\n\n";
+	print "IP\t\tDate\t\tTime\t\tCountry\n";
+	print color 'reset';
 
-print "IP\t\tDate\t\tTime\t\tCountry\n";
-print color 'reset';
-
-while (my $banrow = $currentbans->fetchrow_arrayref) {
-	print $banrow->[0] . "\t" . $banrow->[1] . "\t" . $banrow->[2] . "\t" . $banrow->[3] . "\n";
-	}
-
-print "\n";
+	while (my $banrow = $currentbans->fetchrow_arrayref) {
+		print $banrow->[0] . "\t" . $banrow->[1] . "\t" . $banrow->[2] . "\t" . $banrow->[3] . "\n";
+		}
+	print "\n";
 	}
