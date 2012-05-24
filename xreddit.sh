13f3a261c55c0843a48ab6971b832856abc7e14c
Kyle Berry <4wd22r@gmail.com>
2012-05-24 12:56:03 -0500

    changed display characters

--- xreddit.sh
+++ xreddit.sh
@@ -7,6 +7,6 @@ fi
 curl --connect-timeout 3 -fsm 3 http://www.reddit.com/user/$1/about.json | 
 awk '{match($0, "k_karma\": ([0-9]+)", a);
       match($0, "t_karma\": ([0-9]+)", b);
-      print "L:", a[1], "C:", b[1];}'
+      print "l:", a[1], "c:", b[1];}'
 
 
